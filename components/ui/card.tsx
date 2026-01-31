58fae47e1e3f55a6c8eb6ec7f69d5d87736b3752
Rebelronak <ronakkanani92@gmail.com>
2026-01-31 16:29:50 +0530

    Force rebuild: touch component files

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
+// Card component for dashboard layouts
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
